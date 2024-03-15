259907ff527add50cc5c8a3b88bcd88f284ccecc
Wojtek Kowalczyk <wojtekkowalczyk01@gmail.com>
2024-03-15 18:33:54 +0100

    setup project for visual studio

--- Raytracer/src/main.cpp
+++ Raytracer/src/main.cpp
@@ -2,9 +2,12 @@
 
 #include "vector.h"
 
+#include "tests.h"
+
 int main() 
 {
     // TODO : start from here - primitives are done, consult requirements and code intersections
+    TestVector();
 
     return 0;
 }
