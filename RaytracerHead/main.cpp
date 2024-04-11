e8941fd9d68c3010034ea055db20f79affc00f4b
Wojtek Kowalczyk @manjaro <wojtekkowalczyk01@gmail.com>
2024-04-11 16:25:35 +0200

    disabled antialiasing (because I think its broken and I need fast rendering currently)

--- RaytracerHead/main.cpp
+++ RaytracerHead/main.cpp
@@ -11,29 +11,41 @@
 
 #include "exampleScenes.h"
 
-Color antialias(float x, float y, const Scene& scene, const Camera& camera, float pixelSize, int threshold = 0, bool force = false) {
+Color antialias(float x, float y, const Scene& scene, const Camera& camera, float pixelSize, int threshold = 0, bool force = false) 
+{
+	// TODO : fix antialiasing
+	return scene.TraceRay(camera.ConstructRay(x, y)).Clamped();
 
-	if (threshold >=2 || force) {
+	// pixelSize *= 1.1f;
+
+	if (threshold >= 2 || force)
+	{
 		Ray ray1 = camera.ConstructRay(x, y);
 		Ray ray2 = camera.ConstructRay(x + pixelSize, y);
 		Ray ray3 = camera.ConstructRay(x, y + pixelSize);
 		Ray ray4 = camera.ConstructRay(x + pixelSize, y + pixelSize);
+
+		// Ray ray1 = camera.ConstructRay(x + 0.25f * pixelSize, y + 0.25f * pixelSize);
+		// Ray ray2 = camera.ConstructRay(x + 0.25f * pixelSize, y + 0.75f * pixelSize);
+		// Ray ray3 = camera.ConstructRay(x + 0.75f * pixelSize, y + 0.25f * pixelSize);
+		// Ray ray4 = camera.ConstructRay(x + 0.75f * pixelSize, y + 0.75f * pixelSize);
+
 		Color color1 = scene.TraceRay(ray1).Clamped();
-		Color color2 = scene.TraceRay(ray1).Clamped();
-		Color color3 = scene.TraceRay(ray1).Clamped();
-		Color color4 = scene.TraceRay(ray1).Clamped();
+		Color color2 = scene.TraceRay(ray2).Clamped();
+		Color color3 = scene.TraceRay(ray3).Clamped();
+		Color color4 = scene.TraceRay(ray4).Clamped();
 		Color finalColor = (color1 + color2 + color3 + color4) * 0.25f;
 
 		return finalColor;
 	}
-	else {
+	else 
+	{
 		float halfSize = pixelSize / 2.0f;
 
 		Color color3 = antialias(x, y, scene, camera, halfSize, threshold + 1);
 		Color color1 = antialias(x + halfSize, y, scene, camera, halfSize, threshold + 1);
 		Color color2 = antialias(x, y + halfSize, scene, camera, halfSize, threshold + 1);
 		Color color4 = antialias(x + halfSize, y + halfSize, scene, camera, halfSize, threshold + 1);
-		
 
 		Color finalColor = (color1 + color2 + color3 + color4) * 0.25f;
 
@@ -49,9 +61,7 @@ void RenderScene(const Scene& scene, const Camera& camera, Buffer& target)
 	{
 		for (int x = 0; x < target.GetWidth(); x++)
 		{
-			// TODO : enable antialiasing again
-			// Color finalColor = antialias((float)x, (float)y, scene, camera, pixelSize, 0, true);
-			Color finalColor = scene.TraceRay(camera.ConstructRay(x, y));
+			Color finalColor = antialias((float)x, (float)y, scene, camera, pixelSize, 0, true);
 			target.ColorAt(x, y) = Color::ToInt(finalColor);
 		}	
 	}
